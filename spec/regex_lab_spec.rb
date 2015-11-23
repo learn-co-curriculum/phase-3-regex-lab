701b2e8d1c062063f2afd52b1b84746917f0e1bd
AnnJohn <ann@flatironschool.com>
2015-11-23 15:06:46 -0500

    Fixed typo in specs

--- spec/regex_lab_spec.rb
+++ spec/regex_lab_spec.rb
@@ -64,14 +64,14 @@ describe "Working with Regular expressions" do
     it "returns true for valid phone numbers, regardless of formatting" do
       valid_numbers = ["2438894546", "(718)891-1313", "234 435 9978", "(800)4261134"]
       valid_numbers.each do |number|
-        expect(valid_phone_number(number)).to be(true)
+        expect(valid_phone_number?(number)).to be(true)
       end
     end
 
     it "returns false for invalid phone numbers, regardless of formatting" do
       valid_numbers = ["28894546", "(718)891-13135", "234 43 9978", "(800)IloveNY"]
       valid_numbers.each do |number|
-        expect(valid_phone_number(number)).to be(false)
+        expect(valid_phone_number?(number)).to be(false)
       end
     end
   end
