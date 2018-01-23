3f67d6c316f9b589cdd63e0df8c8920e9ff3a6f1
curiositypaths <curiositypaths@users.noreply.github.com>
2018-01-23 15:20:35 -0500

    Improve words_starting_with_un_and_ending_with_in regular expression

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -5,7 +5,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/un\w+ing\b/)
+  text.scan(/\bun\w*ing\b/)
 end
 
 def words_five_letters_long(text)
