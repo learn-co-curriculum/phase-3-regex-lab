76e91cad5ff37da892bfc41d949e6938c8af60b6
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-08-16 13:19:23 +0300

    commit tree 457bc0d31148a9730914d57d80b362ff3f4dd909

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,25 @@
 def starts_with_a_vowel?(word)
+    reg=word.match(/^[aeiouAEIOU]\w*/)
+    reg ? true : false
 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+    text.scan(/\bun\w*ing\b/)
 
 end
 
 def words_five_letters_long(text)
+    text.scan(/\b\w{5}\b/) 
 
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
+    text.match(/^[A-Z].+[\.!?]$/) ? true : false
 
 end
 
 def valid_phone_number?(phone)
+    phone.match(/([0-9] *){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/) ? true : false
 
 end
