c1b6ca828b3758438bd9a3f6acd3670fe6b00343
chrisphine <onyangoketta@gmail.com>
2022-08-26 23:37:31 +0300

    commit tree c9d5418befadef4a5f9c842d6236e4fab4feb706

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,12 @@
 def starts_with_a_vowel?(word)
-
+  # word.match?(/\A[aeiou]/i)
+  !!word.capitalize.match(/\A+[AEIOU]/)
+  #word.start_with?('a','e','i','o','u')
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+#text.start_with?('un')&.end_with?('ing')
+text.scan(/un\w*ing\b/)
 end
 
 def words_five_letters_long(text)
@@ -11,7 +14,10 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+#text.scan(/\A[.?]/i)
+#text.match?(/\A[.?,]/)
+#!!text.match(/^[A-Z].*\W$/)
+text.match?(/[A-Z]./)
 end
 
 def valid_phone_number?(phone)
